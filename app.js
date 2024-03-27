09428bb21905a9d2b6f11b3ddc986d4630206251
reshad131 <161569107+reshad131@users.noreply.github.com>
2024-03-27 10:01:00 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+ // Task1
+
+// eded=parseInt(prompt("Reqem daxil edin"))
+// SM=true
+
+// for(i=2;eded>i;i++){
+//     if(eded % i ===0){
+//         SM=false;
+//         break
+//     }
+// }
+
+// if(SM){
+//     console.log(eded + "  sade ededdir")
+
+// }else{
+//     console.log(eded + "  murekkeb ededdir")
+// }
+
+// TAsk 2
+
+
+// let eded = parseInt(prompt("Eded daxil edin"));
+// let toplam = 0;
+
+// for (let i = 1; i < eded; i++) {
+//   if (eded % i === 0) {
+//     toplam += i;
+//   }
+// }
+
+// if (toplam === eded && eded !== 0) {
+//   console.log(eded + " mukemmel ededdir");
+// } else {
+//   console.log(eded + " mukemmel eded deyil");
+// }
+
+
+
+
+// TAsk 3
+
+
+// eded=parseInt(prompt("1.reqemi daxil edin"))
+// eded0=parseInt(prompt("2.reqemi daxil edin"))
+
+// for(reqem=eded;eded0>reqem;reqem++){
+//     toplam=0
+
+//     for(i=1;reqem>i;i++){
+//         if(reqem%i===0){
+//             toplam+=i
+//         }
+//     }
+
+//     if(reqem===toplam && reqem!==0){
+//         console.log(i + " Mukemmel ededdir")
+//     }
+// }
+
+  
+// Task 4
+
+// eded=parseInt(prompt("1.reqemi daxil edin"))
+// eded0=parseInt(prompt("2.reqemi daxil edin"))
+// Sade=true
+
+// for(reqem=eded;eded0>reqem;reqem++){
+//     Sade=true
+//     for(i=2;i<reqem;i++){
+//         if(reqem % i ===0){
+//             Sade=false
+//             break
+            
+//         }
+//     }
+//     if(Sade&&reqem>1){
+//         console.log(reqem)
+//     }
+// }
+
+
+    
+// Task 5
+
+// eded=parseInt(prompt("1.reqemi daxil edin"))
+// eded0=parseInt(prompt("2. reqemi daxil edin"))
+
+// for(reqem=eded;reqem<eded0;reqem++){
+//     if(reqem%10==7){
+//         console.log(reqem)
+//     }
+// }
